7158e16c882cc26d2b0fd6e1149c8442398f58ab
Pixely207 <zx147884@gnspes.ca>
2024-11-13 16:18:05 -0400

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -518,6 +518,7 @@ static const u16 sSpeciesToHoennPokedexNum[NUM_SPECIES - 1] =
     SPECIES_TO_HOENN(JIRACHI),
     SPECIES_TO_HOENN(DEOXYS),
     SPECIES_TO_HOENN(CHIMECHO),
+    SPECIES_TO_HOENN(WOBBIGGET),
 };
 
 // Assigns all species to the National Dex Index (Summary No. for National Dex)
@@ -934,6 +935,7 @@ static const u16 sSpeciesToNationalPokedexNum[NUM_SPECIES - 1] =
     SPECIES_TO_NATIONAL(JIRACHI),
     SPECIES_TO_NATIONAL(DEOXYS),
     SPECIES_TO_NATIONAL(CHIMECHO),
+    SPECIES_TO_NATIONAL(WOBBIGGET),
 };
 
 // Assigns all Hoenn Dex Indexes to a National Dex Index
@@ -1141,6 +1143,7 @@ static const u16 sHoennToNationalOrder[NUM_SPECIES - 1] =
     HOENN_TO_NATIONAL(RAYQUAZA),
     HOENN_TO_NATIONAL(JIRACHI),
     HOENN_TO_NATIONAL(DEOXYS),
+    HOENN_TO_NATIONAL(WOBBIGGET),
     HOENN_TO_NATIONAL(BULBASAUR), // Pokémon from here onwards are UNSEEN in the HoennDex.
     HOENN_TO_NATIONAL(IVYSAUR),
     HOENN_TO_NATIONAL(VENUSAUR),
